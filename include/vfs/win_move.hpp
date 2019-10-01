c08819b483557cf2cd74d9ae64175ae6d36b5d9e
Houssem <houssem.achouri@gmail.com>
2019-10-01 12:51:25 -0400

    Added a file::copy function

--- include/vfs/win_move.hpp
+++ include/vfs/win_move.hpp
@@ -25,7 +25,35 @@ namespace vfs {
                 if (++attempts == maxAttempts)
                 {
                     const auto errorCode = GetLastError();
-                    vfs_errorf("MoveFileEx(%s, %s, MOVEFILE_COPY_ALLOWED | MOVEFILE_WRITE_THROUGH) failed after %d attempts, returned error: %s", src.c_str(), dst.c_str(), attempts, get_last_error_as_string(errorCode).c_str());
+                    vfs_errorf("MoveFileEx(%s, %s, MOVEFILE_COPY_ALLOWED | MOVEFILE_WRITE_THROUGH) failed after %d attempts, returned error: %s",
+                        src.c_str(), dst.c_str(), attempts, get_last_error_as_string(errorCode).c_str());
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //----------------------------------------------------------------------------------------------
+        // Moves a file or a directory from src path to dst path.
+        // If src path is a directory, src and dst paths must be on the same drive.
+        static bool copy(const path &src, const path &dst, bool overwrite = false, int32_t maxAttempts = 1)
+        {
+            // When moving a file, the destination can be on a different file system or volume.
+            // If the destination is on another drive, you must set the MOVEFILE_COPY_ALLOWED flag in dwFlags.
+            auto flags = MOVEFILE_COPY_ALLOWED | MOVEFILE_WRITE_THROUGH;
+            if (overwrite)
+            {
+                flags |= MOVEFILE_REPLACE_EXISTING;
+            }
+
+            auto attempts = 0;
+            while (CopyFile(src.c_str(), dst.c_str(), overwrite ? FALSE : TRUE) == FALSE)
+            {
+                if (++attempts == maxAttempts)
+                {
+                    const auto errorCode = GetLastError();
+                    vfs_errorf("CopyFile(%s, %s, %d) failed after %d attempts, returned error: %s",
+                        src.c_str(), dst.c_str(), overwrite, attempts, get_last_error_as_string(errorCode).c_str());
                     return false;
                 }
             }
