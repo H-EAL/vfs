9b2f3051f5f7b89d97ee66bd6a9cd9fd03bcb6c3
nathalieraffray <nathalie@3dverse.com>
2021-07-05 21:56:28 -0400

    Fixed issues with shared_memory_tests
    posix and windows have conflicting rules for shared memory names
    Made sure the shared memory we create is valid before writing to it or doing any other tests

--- tests/shared_memory_tests.hpp
+++ tests/shared_memory_tests.hpp
@@ -2,19 +2,22 @@
 TEST_CASE("Shared memory.", "[sharedmemory]")
 {
     // NOTE: For each SECTION the TEST_CASE is executed from the start.
-
-    const auto sharedMemoryName = "mrMemory";
-    auto spSharedMemory = vfs::create_shared_memory(sharedMemoryName, text.size());
-
-    // Write.
-    memcpy(spSharedMemory->cursor(), text.data(), text.size());
-
     SECTION("we can create shared memory")
     {
+        // In posix, shared memory must be prefaced by a slash, but in windows a slash in the name is invalid.
+#if VFS_PLATFORM_WIN
+        const auto sharedMemoryName = "mrsMemory";
+#elif VFS_PLATFORM_POSIX
+        const auto sharedMemoryName = "/mrsMemory";
+#endif
+        auto spSharedMemory = vfs::create_shared_memory(sharedMemoryName, text.size());
         REQUIRE(spSharedMemory->isValid());
 
-        SECTION("we can read and write to it")
+        SECTION("we can read and write to shared memory")
         {
+            // Write.
+            memcpy(spSharedMemory->cursor(), text.data(), text.size());
+
             // Read.
             std::vector<uint8_t> textRead;
             textRead.resize(text.size());
@@ -22,40 +25,47 @@ TEST_CASE("Shared memory.", "[sharedmemory]")
 
             REQUIRE(memcmp(textRead.data(), text.data(), text.size()) == 0);
         }
-    }
 
-    SECTION("we can open an existing shared memory object")
-    {
-        auto spOpenedMemory = vfs::open_shared_memory(sharedMemoryName);
-        // Interestingly you are opening memory, but it's not mapped to the exact same place. i.e. spOpenedMemory->cursor() != spSharedMemory->cursor()
-        auto isValid = spOpenedMemory->isValid();
-        REQUIRE(isValid);
-
-        SECTION("we can read from this existing shared memory")
+        SECTION("we can open an existing shared memory object")
         {
-            std::vector<uint8_t> textRead;
-            textRead.resize(text.size());
-            memcpy(textRead.data(), spOpenedMemory->cursor(), text.size());
+            // Write to the existing shared memory.
+            memcpy(spSharedMemory->cursor(), text.data(), text.size());
 
-            REQUIRE(memcmp(textRead.data(), text.data(), text.size()) == 0);
+            auto spOpenedMemory = vfs::open_shared_memory(sharedMemoryName);
+            // Interestingly you are opening memory, but it's not mapped to the exact same place. i.e. spOpenedMemory->cursor() != spSharedMemory->cursor()
+            REQUIRE(spOpenedMemory->isValid());
+
+            SECTION("we can read from this existing shared memory")
+            {
+                std::vector<uint8_t> textRead;
+                textRead.resize(text.size());
+                memcpy(textRead.data(), spOpenedMemory->cursor(), text.size());
+
+                REQUIRE(memcmp(textRead.data(), text.data(), text.size()) == 0);
+            }
+
+            SECTION("we can write to this existing shared memory")
+            {
+                REQUIRE(text.size() == text2.size());
+                memcpy(spOpenedMemory->cursor(), text2.data(), text2.size());
+
+                std::vector<uint8_t> textRead;
+                textRead.resize(text2.size());
+                memcpy(textRead.data(), spSharedMemory->cursor(), text2.size());
+
+                REQUIRE(memcmp(textRead.data(), text2.data(), text2.size()) == 0);
+            }
         }
 
-        SECTION("we can write to this existing shared memory")
+        SECTION("we cannot open a non-existing shared memory object")
         {
-            vfs_check(text.size() == text2.size());
-            memcpy(spOpenedMemory->cursor(), text2.data(), text2.size());
-
-            std::vector<uint8_t> textRead;
-            textRead.resize(text2.size());
-            memcpy(textRead.data(), spSharedMemory->cursor(), text2.size());
-
-            REQUIRE(memcmp(textRead.data(), text2.data(), text2.size()) == 0);
+#if VFS_PLATFORM_WIN
+            const auto openSharedMemoryName = "mrMemory";
+#elif VFS_PLATFORM_POSIX
+            const auto openSharedMemoryName = "/mrMemory";
+#endif
+            auto spOpenedMemory = vfs::open_shared_memory(openSharedMemoryName, text.size());
+            REQUIRE(!spOpenedMemory->isValid());
         }
-    }
-
-    SECTION("we cannot open a non-existing shared memory object")
-    {
-        auto spOpenedMemory = vfs::open_shared_memory("/mrsMemory", text.size());
-        REQUIRE(!spOpenedMemory->isValid());
     }
 }
